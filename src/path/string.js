efb300b3d1714d51e6ae1875f876326a15621d9f
Philippe Rivière <fil@rezo.net>
2019-11-12 10:29:02 +0100

    lebab --replace 'src/**/*.js' --transform template

--- src/path/string.js
+++ src/path/string.js
@@ -52,8 +52,5 @@ PathString.prototype = {
 };
 
 function circle(radius) {
-  return "m0," + radius
-      + "a" + radius + "," + radius + " 0 1,1 0," + -2 * radius
-      + "a" + radius + "," + radius + " 0 1,1 0," + 2 * radius
-      + "z";
+  return `m0,${radius}a${radius},${radius} 0 1,1 0,${-2 * radius}a${radius},${radius} 0 1,1 0,${2 * radius}z`;
 }
