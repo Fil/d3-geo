c8ecff2377c4ca86f1029428dcb6ac5e35870ef1
Philippe Rivière <fil@rezo.net>
2019-11-12 10:23:43 +0100

    fix for let

--- src/centroid.js
+++ src/centroid.js
@@ -69,7 +69,8 @@ function centroidLinePoint(lambda, phi) {
   const x = cosPhi * cos(lambda);
   const y = cosPhi * sin(lambda);
   const z = sin(phi);
-  let w = atan2(sqrt((w = y0 * z - z0 * y) * w + (w = z0 * x - x0 * z) * w + (w = x0 * y - y0 * x) * w), x0 * x + y0 * y + z0 * z);
+  let w;
+  w = atan2(sqrt((w = y0 * z - z0 * y) * w + (w = z0 * x - x0 * z) * w + (w = x0 * y - y0 * x) * w), x0 * x + y0 * y + z0 * z);
   W1 += w;
   X1 += w * (x0 + (x0 = x));
   Y1 += w * (y0 + (y0 = y));
